3a1bd4876344d525ca37b1f24c6de8755b7e1fc8
Richie Bendall <richiebendall@gmail.com>
2020-10-31 23:45:23 +1300

    Fix calculation
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -12,5 +12,19 @@ module.exports = ([startX, startY], [endX, endY]) => {
 	assertNumber(endX)
 	assertNumber(endY)
 
-	return Math.atan2(endX - startX, endY - startY) * 180 / Math.PI
+	if ((startX === endX) && (startY === endY)) {
+		return Number.NaN
+	}
+
+	const angle = Math.atan2(endY - startY, endX - startX) * 180 / Math.PI
+
+	if (angle < 0) {
+		return -angle + 90
+	}
+
+	if (angle <= 90) {
+		return 90 - angle
+	}
+
+	return 360 - (angle - 90)
 }
