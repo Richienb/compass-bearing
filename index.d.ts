e7d26f5acd70d61263ca2db1cbb5ae1317b75537
Richie Bendall <richiebendall@gmail.com>
2020-11-18 19:19:48 +1300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 Get the [relative](https://en.wikipedia.org/wiki/Relative_bearing) [compass bearing](https://en.wikipedia.org/wiki/Bearing_(navigation)) of a set of coordinates from another set.
 
 @param startCoordinates The starting x and y coordinate pair to get the relative bearing from.
-@param postfix The ending x and y coordinate pair to get the relative bearing from.
+@param endCoordinates The ending x and y coordinate pair to get the relative bearing from.
 @returns A degree from `0` up to (but excluding) `360`.
 
 @example
